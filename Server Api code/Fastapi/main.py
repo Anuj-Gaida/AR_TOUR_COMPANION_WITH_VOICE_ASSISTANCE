607b17de605d6b3dabd28de8bcf59003d30594b9
Anuj-Gaida <gaidaanuj@gmail.com>
2024-07-26 11:34:48 +0545

    add comment

--- Server Api code/Fastapi/main.py
+++ Server Api code/Fastapi/main.py
@@ -3,6 +3,7 @@ from socket import gethostname,gethostbyname
 from sys import path  as sys_path
 from os import path as os_path
 
+##comment
 ##
 sys_path.append(os_path.abspath('../model/'))
 sys_path.append(os_path.abspath('../../model/'))
