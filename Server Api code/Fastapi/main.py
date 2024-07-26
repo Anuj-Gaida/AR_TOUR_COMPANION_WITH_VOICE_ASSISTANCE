2d0a5cb9652830392ff579cfb8b993d96edddcef
Anuj-Gaida <gaidaanuj@gmail.com>
2024-07-26 11:33:06 +0545

    add comment

--- Server Api code/Fastapi/main.py
+++ Server Api code/Fastapi/main.py
@@ -2,6 +2,8 @@ import uvicorn
 from socket import gethostname,gethostbyname
 from sys import path  as sys_path
 from os import path as os_path
+
+##
 sys_path.append(os_path.abspath('../model/'))
 sys_path.append(os_path.abspath('../../model/'))
 sys_path.append(os_path.abspath('../model/ImageClassification/'))
